e346601be5869daecd14d8637a7ab8b7aeefe90d
BenitoNat <natalebenito@libero.it>
2022-08-22 09:09:10 +0200

    add gitignore and gitattributes

---
+++ Source/AntoneDeChiaraUdemy/AntoneDeChiaraUdemyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AntoneDeChiaraUdemyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANTONEDECHIARAUDEMY_API AAntoneDeChiaraUdemyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
